c8926e36accdcb54ce6281ea96bd796e09c19d64
joeyfasanelli <joeyfas1704@gmail.com>
2023-06-01 11:29:08 -0400

    update opacity

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
-            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain opacity-60 transition duration-300 ease-in-out hover:opacity-100" />
+            <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain opacity-80 transition duration-300 ease-in-out hover:opacity-100" />
           </div>
         ))}
       </div>
